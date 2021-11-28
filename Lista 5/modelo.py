576ad5dae0c737085f8126fbc91432132cc0ce6b
anderlr13 <anderlr13@gmail.com>
2021-11-28 19:40:32 -0300

    lista 5

--- Lista 5/modelo.py
+++ Lista 5/modelo.py
@@ -13,12 +13,15 @@ import numpy as np
 import OpenGL.GL as gl
 import OpenGL.GLUT as glut
 from ctypes import c_void_p
+import utils as ut
 
 from numpy.lib.function_base import angle
 
 # contador de teclas
 key_counter = 0
 
+# opcao inicial
+option = 't'
 # Largura da Janela
 win_width = 800
 # Altura da Janela
@@ -36,7 +39,7 @@ EBO = None
 
 # Vertex shader.
 vertex_code = """
-#version 330 core
+# version 330 core
 layout (location = 0) in vec3 position;
 layout (location = 1) in vec3 color;
 
@@ -51,7 +54,7 @@ void main()
 
 # Fragment shader.
 fragment_code = """
-#version 330 core
+# version 330 core
 
 in vec3 vColor;
 out vec4 FragColor;
@@ -59,20 +62,48 @@ out vec4 FragColor;
 void main()
 {
     FragColor = vec4(vColor, 1.0f);
-} 
+}
 """
 
 
 def display():
 
-    gl.glClearColor(0.0, 0.0, 0.0, 1.0)
-    gl.glClear(gl.GL_COLOR_BUFFER_BIT)
+    gl.glClearColor(0.2, 0.3, 0.3, 1.0)
+    gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
 
     gl.glUseProgram(program)
     gl.glBindVertexArray(VAO)
+
+    # Define model matrix.
+    Rx = ut.matRotateX(np.radians(-30.0))
+    T = ut.matTranslate(0.0, 0.0, -1.0)
+    model = np.matmul(T, Rx)
+
+    # Retrieve location of model variable in shader.
+    loc = gl.glGetUniformLocation(program, "model")
+    # Send matrix to shader.
+    gl.glUniformMatrix4fv(loc, 1, gl.GL_FALSE, model.transpose())
+
+    # Define view matrix.
+    view = ut.matTranslate(0.0, 0.0, -3.0)
+
+    # Retrieve location of view variable in shader.
+    loc = gl.glGetUniformLocation(program, "view")
+    # Send matrix to shader.
+    gl.glUniformMatrix4fv(loc, 1, gl.GL_FALSE, view.transpose())
+
+    # Define projection matrix.
+    projection = ut.matPerspective(np.radians(
+        0.1), win_width/win_height, 0.1, 100.0)
+
+    # Retrieve location of projection variable in shader.
+    loc = gl.glGetUniformLocation(program, "projection")
+    # Send matrix to shader.
+    gl.glUniformMatrix4fv(loc, 1, gl.GL_FALSE, projection.transpose())
+
     # Draws the triangle.
     gl.glDrawArrays(gl.GL_TRIANGLES, 0, 6 * total_pixels)
-    #gl.glDrawArrays(gl.GL_POINTS, 0, 6 * total_pixels)
+
     glut.glutSwapBuffers()
 
 
@@ -97,49 +128,95 @@ def reshape(width, height):
 def keyboard(key, x, y):
 
     global key_counter
+    global option
 
-    # change type
+    # change type of drawing
     if key == b'v' and key_counter == 0:
         gl.glPolygonMode(gl.GL_FRONT_AND_BACK, gl.GL_POINT)
         key_counter = 1
     elif key == b'v' and key_counter == 1:
         gl.glPolygonMode(gl.GL_FRONT_AND_BACK, gl.GL_LINE)
+        key_counter = 2
+    elif key == b'v' and key_counter == 2:
+        gl.glPolygonMode(gl.GL_FRONT_AND_BACK, gl.GL_FILL)
         key_counter = 0
 
-    # translation
-    elif key == b'a':
-        vertices[:, 0] = vertices[:, 0] - 0.025
-    elif key == b'd':
-        vertices[:, 0] = vertices[:, 0] + 0.025
-    elif key == b'w':
-        vertices[:, 1] = vertices[:, 1] + 0.025
-    elif key == b's':
-        vertices[:, 1] = vertices[:, 1] - 0.025
+    # change option
+    if key == b't':  # translation
+        option = 't'
+    elif key == b'r':  # rotation
+        option = 'r'
+    elif key == b'e':  # scale
+        option = 'e'
 
-    # zoom
-    elif key == b'i':
-        vertices[:, 0] = vertices[:, 0] * 1.025
-        vertices[:, 1] = vertices[:, 1] * 1.025
-    elif key == b'o':
-        vertices[:, 0] = vertices[:, 0] / 1.025
-        vertices[:, 1] = vertices[:, 1] / 1.025
+    if option == 't':
+        if key == b'w':
+            vertices[:, 1] = vertices[:, 1] + 0.025
+        elif key == b's':
+            vertices[:, 1] = vertices[:, 1] - 0.025
+        elif key == b'd':
+            vertices[:, 0] = vertices[:, 0] + 0.025
+        elif key == b'a':
+            vertices[:, 0] = vertices[:, 0] - 0.025
+        elif key == b'd':
+            vertices[:, 0] = vertices[:, 0] + 0.025
+        elif key == b'p':
+            vertices[:, 2] = vertices[:, 2] + 0.025
+            vertices[:, 1] = vertices[:, 1] + 0.025
+            vertices[:, 0] = vertices[:, 0] + 0.025
+        elif key == b'n':
+            vertices[:, 2] = vertices[:, 2] - 0.025
+            vertices[:, 1] = vertices[:, 1] - 0.025
+            vertices[:, 0] = vertices[:, 0] - 0.025
 
-    # rotation
-    # x = x1 * cos(theta) + y1 * sin(theta)
-    # y = y1 * cos(theta) - x1 * sin(theta)
-    elif key == b'p':
-        vertices[:, 0] = vertices[:, 0] * \
-            np.cos(0.01) - vertices[:, 1] * np.sin(0.01)
-        vertices[:, 1] = vertices[:, 1] * \
-            np.cos(0.01) + vertices[:, 0] * np.sin(0.01)
-    elif key == b'n':
-        vertices[:, 0] = vertices[:, 0] * \
-            np.cos(-0.01) - vertices[:, 1] * np.sin(-0.01)
-        vertices[:, 1] = vertices[:, 1] * \
-            np.cos(-0.01) + vertices[:, 0] * np.sin(-0.01)
+    elif option == 'r':
+        if key == b'p':  # giro em torno de z +
+            vertices[:, 0] = vertices[:, 0] * \
+                np.cos(0.01) + vertices[:, 1] * np.sin(0.01)
+            vertices[:, 1] = vertices[:, 1] * \
+                np.cos(0.01) - vertices[:, 0] * np.sin(0.01)
+        elif key == b'n':  # giro em torno de z -
+            vertices[:, 0] = vertices[:, 0] * \
+                np.cos(-0.01) + vertices[:, 1] * np.sin(-0.01)
+            vertices[:, 1] = vertices[:, 1] * \
+                np.cos(-0.01) - vertices[:, 0] * np.sin(-0.01)
+        elif key == b'd':  # giro em torno de x +
+            vertices[:, 1] = vertices[:, 1] * \
+                np.cos(0.01) + vertices[:, 2] * np.sin(0.01)
+            vertices[:, 2] = vertices[:, 2] * \
+                np.cos(0.01) - vertices[:, 1] * np.sin(0.01)
+        elif key == b'a':  # giro em torno de x -
+            vertices[:, 1] = vertices[:, 1] * \
+                np.cos(-0.01) + vertices[:, 2] * np.sin(-0.01)
+            vertices[:, 2] = vertices[:, 2] * \
+                np.cos(-0.01) - vertices[:, 1] * np.sin(-0.01)
+        elif key == b'w':  # giro em torno de y +
+            vertices[:, 0] = vertices[:, 0] * \
+                np.cos(0.01) + vertices[:, 2] * np.sin(0.01)
+            vertices[:, 2] = vertices[:, 2] * \
+                np.cos(0.01) - vertices[:, 0] * np.sin(0.01)
+        elif key == b's':  # giro em torno de y +
+            vertices[:, 0] = vertices[:, 0] * \
+                np.cos(-0.01) + vertices[:, 2] * np.sin(-0.01)
+            vertices[:, 2] = vertices[:, 2] * \
+                np.cos(-0.01) - vertices[:, 0] * np.sin(-0.01)
+
+    elif option == 'e':
+        if key == b'w':
+            vertices[:, 1] = vertices[:, 1] * 1.025
+        elif key == b's':
+            vertices[:, 1] = vertices[:, 1] / 1.025
+        elif key == b'd':
+            vertices[:, 0] = vertices[:, 0] * 1.025
+        elif key == b'a':
+            vertices[:, 0] = vertices[:, 0] / 1.025
+        elif key == b'p':
+            vertices[:, 2] = vertices[:, 2] * 1.025
+        elif key == b'n':
+            vertices[:, 2] = vertices[:, 2] / 1.025
 
     # reset
-    elif key == b'm':
+    if key == b'm':
         vertices[:, :] = create_image()
 
     initData(vertices)
@@ -208,6 +285,11 @@ def create_image():
     vertices = np.concatenate(vertices, axis=0)
     vertices = np.array(vertices).reshape(len(vertices)//6, 6)
 
+    # Ajustando escala inicial
+    vertices[:, 0] = vertices[:, 0] / 1.25
+    vertices[:, 1] = vertices[:, 1] / 1.25
+    vertices[:, 2] = vertices[:, 2] / 1.25
+
     return vertices
 
 
@@ -221,6 +303,8 @@ def initData(vertices):
     #     0, 1, 3,  # first Triangle
     #     1, 2, 3   # second Triangle
     # ], dtype='float32')
+
+    gl.glEnable(gl.GL_DEPTH_TEST)
 
     # Vertex array.
     VAO = gl.glGenVertexArrays(1)
@@ -247,6 +331,7 @@ def initData(vertices):
 
     # Unbind Vertex Array Object.
     gl.glBindVertexArray(0)
+
 
 # Create program (shaders).
 #
@@ -304,7 +389,8 @@ def main():
     glut.glutInit()
     glut.glutInitContextVersion(3, 3)
     glut.glutInitContextProfile(glut.GLUT_CORE_PROFILE)
-    glut.glutInitDisplayMode(glut.GLUT_DOUBLE | glut.GLUT_RGBA)
+    glut.glutInitDisplayMode(
+        glut.GLUT_DOUBLE | glut.GLUT_RGBA | glut.GLUT_DEPTH)
     glut.glutInitWindowSize(win_width, win_height)
     glut.glutCreateWindow('Triangle')
 
